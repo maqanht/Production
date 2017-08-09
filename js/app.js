fb36bf07dcb2bde1338a0d60717dffd4a9a2bdc1
Hatim Dahodwala <v-hadah@microsoft.com>
2017-08-09 19:16:26 +0530

    Added AngularJs for routing.

---
+++ js/app.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('MAQSoftwareApp', ['ngRoute', 'ngResource']);
+app.config(function ($routeProvider,$locationProvider) {
+    $locationProvider.html5Mode(true)
+    $routeProvider
+    .when("/", {
+        templateUrl: "/views/home.html"
+    })
+    .when('/expertise/datamanagement', {
+        templateUrl: '/views/datamanagement.html',
+    })
+    .when('/expertise/artificialintelligence', {
+        templateUrl: '/views/artificialintelligence.html',
+    })
+    $routeProvider.otherwise({ redirectTo: "/" });
+
+});
