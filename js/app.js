2b10c851c62ebecb785404191a39efba5ebe47a1
Hardik Avaiya <v-harav@microsoft.com>
2017-08-10 20:11:36 +0530

    added engagment pages

--- js/app.js
+++ js/app.js
@@ -8,18 +8,42 @@ app.config(function ($routeProvider, $locationProvider) {
     .when("/", {
         templateUrl: "/views/home.html",
         controller: "HomeController"
-    }).when('/expertise/datamanagement', {
+    })
+    .when('/expertise', {
         templateUrl: '/views/datamanagement.html'
-    }).when('/expertise/artificialintelligence', {
+    })
+    .when('/expertise/datamanagement', {
+        templateUrl: '/views/datamanagement.html'
+    })
+    .when('/expertise/artificialintelligence', {
         templateUrl: '/views/artificialintelligence.html'
-    }).when('/expertise/selfservicebi', {
+    })
+    .when('/expertise/selfservicebi', {
         templateUrl: '/views/selfservicebi.html'
-    }).when('/expertise/appdevelopment', {
+    })
+    .when('/expertise/appdevelopment', {
         templateUrl: '/views/appdevelopment.html'
-    }).when('/expertise/cloudtransformation', {
+    })
+    .when('/expertise/cloudtransformation', {
         templateUrl: '/views/cloudtransformation.html'
-    }).when('/expertise/collaborationcontent', {
+    })
+    .when('/expertise/collaborationcontent', {
         templateUrl: '/views/collaborationcontent.html'
+    })
+    .when('/engagement', {
+        templateUrl: '/views/about.html'
+    })
+    .when('/engagement/about', {
+        templateUrl: '/views/about.html'
+    })
+    .when('/engagement/deliveryapproach', {
+        templateUrl: '/views/deliveryapproach.html'
+    })
+    .when('/engagement/recognitions', {
+        templateUrl: '/views/recognitions.html'
+    })
+    .when('/engagement/clients', {
+        templateUrl: '/views/clients.html'
     });
     $routeProvider.otherwise({ redirectTo: "/" });
 
